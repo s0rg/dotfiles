5b2fe55e26e9eed1c169c24d9b99825071c86b58
Shevchenko Alexey <al3x.s0rg@gmail.com>
2021-10-19 02:32:10 +0300

    stored at: 2021-10-19 02:32:10+03:00

--- .local/scripts/term_colors.py
+++ .local/scripts/term_colors.py
@@ -1,15 +1,20 @@
 def _mkcolor(code):
-    def _inner(value, bold=False):
+    def _inner(value, bold=False, underline=False):
         head = "\x1B["
         if bold:
             head += "1;"
+        if underline:
+            head += "4;"
         return head + code + "m" + str(value) + "\x1B[0m"
 
     return _inner
 
 
-gray = _mkcolor("30")
+black = _mkcolor("30")
 red = _mkcolor("31")
 green = _mkcolor("32")
+brown = _mkcolor("33")
 blue = _mkcolor("34")
-white = _mkcolor("37")
+purple = _mkcolor("35")
+cyan = _mkcolor("36")
+gray = _mkcolor("37")
